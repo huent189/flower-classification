83e5e63bb55bdb9b532d3380d273c73e2d511a36
huent189 <hueahgase189@gmail.com>
2020-08-15 15:27:48 +0000

    add custom dataset

---
+++ model/CustomDataset.py
@@ -0,0 +1,13 @@
+from torch.utils.data import Dataset
+class CustomDataset(Dataset):
+    def __init__(self, dataset, transform = None):
+        self.dataset = dataset
+        self.transform = transform
+    def __getitem__(self, index):
+        if self.transform:
+            x = self.transform(self.dataset[index][0])
+        else:
+            x = self.dataset[index][0]
+        return x, self.dataset[index][1]
+    def __len__(self):
+        return len(self.dataset)
